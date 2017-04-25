c305e3e249facf171417411453b022628ae17fef
George Higbie <gehigbie@gmail.com>
2017-04-25 16:03:10 -0700

    Add mongo-find to play with fetching data

--- playground/mongo-connect.js
+++ playground/mongo-connect.js
@@ -1,5 +1,6 @@
-const {MongoClient} = require("mongodb");
-
+const {MongoClient, ObjectID} = require("mongodb");
+let obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     if(err){
