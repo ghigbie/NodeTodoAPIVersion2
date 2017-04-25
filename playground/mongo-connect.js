0e5cc40dd94ba2c9eb684bc3d1a0ef9a3f3b6263
George Higbie <gehigbie@gmail.com>
2017-04-25 15:55:34 -0700

    Add object destructuring example

--- playground/mongo-connect.js
+++ playground/mongo-connect.js
@@ -7,18 +7,18 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     }
     console.log("Connected to MongoDB server");
 
-    db.collection("Todos").insertOne({
-        text: "Something that is important to do",
-        completed: false
-    }, (err, result) => {
-        if(err){
-            return console.log("Unable to insert todo", err);
-        }
-        console.log(JSON.stringify(result.opps, undefined, 2));
-    });
+    // db.collection("Todos").insertOne({
+    //     text: "Something that is important to do",
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log("Unable to insert todo", err);
+    //     }
+    //     console.log(JSON.stringify(result.opps, undefined, 2));
+    // });
 
     db.collection("Users").insertOne({
-        name: "Mike",
+        name: "Sammy",
         age: 65,
         location: "New York, NY"
     }, (err, result) => {
