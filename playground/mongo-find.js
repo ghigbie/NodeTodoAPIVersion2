cbb52aa0d0f80e0f29534907bf2c746e4868aa8b
George Higbie <gehigbie@gmail.com>
2017-04-25 16:36:51 -0700

    Fix promise

--- playground/mongo-find.js
+++ playground/mongo-find.js
@@ -6,9 +6,9 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     }
     console.log("Connected to MongoDB server");
 
-    db.collection("Todos").find({completed: false}).count()).then((docs) => {
+    db.collection("Todos").find({completed: false}).count().then((count) => {
         console.log(`Todos count: ${count}`);
-        console.log(JSON.stringify(docs, undefined, 2));
+        console.log(JSON.stringify(count, undefined, 2));
     }, (err) => {
         console.log("Unable to fetch todos", err);
     });
