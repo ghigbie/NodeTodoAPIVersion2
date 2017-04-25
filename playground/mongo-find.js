cc1374f7a5c576ce261c4026e4df2d252bbc5ce2
George Higbie <gehigbie@gmail.com>
2017-04-25 16:51:50 -0700

    Remove print line and add method to print users

--- playground/mongo-find.js
+++ playground/mongo-find.js
@@ -15,7 +15,13 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
 
     db.collection("Users").find({name: "Mike"}).count().then((count) => {
         console.log(`Users count: ${count}`);
-        console.log(JSON.stringify(count, undefined, 2));
+    }, (err) => {
+        console.log("Unable to fetch users", err);
+    });
+
+    db.collection("Users").find({name: "Paul").toArray().then((docs) => {
+        console.log(`Users with the name you are looking for:`);
+        console.log(JOSN.stringify(docs, undefined, 2));
     }, (err) => {
         console.log("Unable to fetch users", err);
     });
