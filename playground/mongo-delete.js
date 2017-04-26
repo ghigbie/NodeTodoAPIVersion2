ff42ba1ba8d428d429d7b37d4b2026e8f157148e
George Higbie <gehigbie@gmail.com>
2017-04-25 17:47:19 -0700

    Add new findOneAndDeleteMethod

--- playground/mongo-delete.js
+++ playground/mongo-delete.js
@@ -10,6 +10,9 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     // db.collection("Todos").deleteMany({text: "Eat Lunch"}).then((result) => {
     //     console.log(result);
     // });
+    // db.collection("Users").deleteMany({name: "Paul"}).then((results) => {
+    //     console.log(results);
+    // });
 
     //deleteOne()
     // db.collection("Todos").deleteOne({text: "Pet cat"}).then((result) => {
@@ -19,7 +22,9 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     //findOneAndDelete()
     // db.collection("Todos").findOneAndDelete({completed: false}).then((result) => {
     //     console.log(result);
-    });
+    //});
+    db.collection("Users").findOneAndDelete({_id: new ObjectID("58ffcf54ca15aa1464d578c8")}).then((results) =>
+        console.log(JSON.stringify(results, undefined, 2)));
 
     //db.close();
 
