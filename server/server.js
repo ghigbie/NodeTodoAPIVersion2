ee19d163c2e0940e45fb359963309a0463fd4425
George Higbie <gehigbie@gmail.com>
2017-04-27 12:31:20 -0700

    Create todo model

--- server/server.js
+++ server/server.js
@@ -2,3 +2,15 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise; //tells mongoose to use promises
 mongoose.connect("mongodb://localhost:27017/TodoApp");
+
+let Todo = mongoose.model("Todo", {
+    text: {
+        type: String
+    },
+    completed:{
+        type: boolean
+    },
+    completedAt:{
+        type: Number
+    }
+});
