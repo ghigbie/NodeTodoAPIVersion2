fda5e729fabbf3156c19301bc57f9ffa6f9c6922
George Higbie <gehigbie@gmail.com>
2017-04-27 11:36:03 -0700

    Add connection to mongo

---
+++ playground/mongo-update.js
@@ -0,0 +1,8 @@
+const {MongoClient, ObjectID} = require("Mongodb");
+
+MongoClient.connect("mongodb://localhost/27017/TodoApp", (err, db) => {
+    if(err){
+        return Console.log("Unable to connect to DB");
+    }
+    console.log("Connected to MongoDB server!!!");
+});
