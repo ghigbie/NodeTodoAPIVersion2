a950506ca5b5c853e0b8e26cdd19d60d1d2e974d
George Higbie <gehigbie@gmail.com>
2017-04-27 11:58:20 -0700

    Add method to update users and todos

--- playground/mongo-update.js
+++ playground/mongo-update.js
@@ -5,4 +5,37 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
         return Console.log("Unable to connect to DB");
     }
     console.log("Connected to MongoDB server!!!");
+
+    db.collection("Todos").findOneAndUpdate({
+            _id: new ObjectID("58ffe36bd56fdd1622110c51")
+        },
+        {
+            $set:{
+            completed: true
+            }
+        },
+        {
+            returnOriginal: false
+        }).then((result)=>{
+            console.log(result);
+        });
+
+    db.collection("Users").findOneAndUpdate({
+        name: "Sammy"
+    },
+    {
+        $set: {
+            name: "Kristi"
+        },
+        $inc: {
+            age: 1
+        }
+    },
+    {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    //db.close();
 });
