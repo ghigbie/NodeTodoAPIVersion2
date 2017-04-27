2efb37401b2951af3844a04c87d3868d5e4a69f6
George Higbie <gehigbie@gmail.com>
2017-04-27 16:02:10 -0700

    Import models

--- server/server.js
+++ server/server.js
@@ -1,62 +1,64 @@
-cosnt {mongoose} = require("./db/mongoose");
+const {mongoose} = require("./db/mongoose");
+const {Todo} = require("./models/todo");
+const {User} = require("./models/user");
 
 
-let Todo = mongoose.model("Todo", {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    },
-    completedAt:{
-        type: Number
-    }
-});
-
-let newTodo = new Todo({
-    text: "Cook dinner for me!",
-    completed: false,
-});
-
-let newTodo1 = new Todo({
-    text: "Walk the doggie!!!",
-    completed: false,
-    completedAt: 12
-});
-
-newTodo.save().then((doc) => {
-    console.log("Saved todo", doc);
-}, (e) => {
-    console.log("Unable to save todo.");
-});
-
-newTodo1.save().then((doc) => {
-    console.log("Saved todo", doc);
-}, (e) => {
-    console.log("Unable to save todo");
-    console.log(e);
-});
-
-let User = mongoose.model("User", {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
-});
-
-let newUser = new User({
-    email: "bob@bob.com"
-});
-
-newUser.save().then((doc) => {
-    console.log("Saved user", doc);
-}, (e) => {
-    console.log("Unable to save user");
-    console.log(e);
-});
+// let Todo = mongoose.model("Todo", {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed:{
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt:{
+//         type: Number
+//     }
+// });
+//
+// let newTodo = new Todo({
+//     text: "Cook dinner for me!",
+//     completed: false,
+// });
+//
+// let newTodo1 = new Todo({
+//     text: "Walk the doggie!!!",
+//     completed: false,
+//     completedAt: 12
+// });
+//
+// newTodo.save().then((doc) => {
+//     console.log("Saved todo", doc);
+// }, (e) => {
+//     console.log("Unable to save todo.");
+// });
+//
+// newTodo1.save().then((doc) => {
+//     console.log("Saved todo", doc);
+// }, (e) => {
+//     console.log("Unable to save todo");
+//     console.log(e);
+// });
+//
+// let User = mongoose.model("User", {
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 1
+//     }
+// });
+//
+// let newUser = new User({
+//     email: "bob@bob.com"
+// });
+//
+// newUser.save().then((doc) => {
+//     console.log("Saved user", doc);
+// }, (e) => {
+//     console.log("Unable to save user");
+//     console.log(e);
+// });
