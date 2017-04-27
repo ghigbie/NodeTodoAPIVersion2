d6a513ee9fda1d69826c13456a349ac226f7500c
George Higbie <gehigbie@gmail.com>
2017-04-27 16:53:28 -0700

    Add status message

--- server/server.js
+++ server/server.js
@@ -10,16 +10,27 @@ const {mongoose} = require("./db/mongoose"),
 app.use(bodyParser.json());//this middleware allows JSON to be sent to express application
 
 app.post("/todos", (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+        console.log("The todo was saved: ", doc);
+    }, (e) => {
+        res.status(400).send(e);
+        console.log("There was an error: ", e);
+    });
     console.log(req.body);
 });
 
-app.get("*", (req, res) => {
-    res.send("<h1>This page cannot be found</h1>");
-});
+// app.get("*", (req, res) => {
+//     res.send("<h1>This page cannot be found</h1>");
+// });
 
 let port = 3000
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on ${port}.`);
 });
 
 // let Todo = mongoose.model("Todo", {
