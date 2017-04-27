c231f7f0e43f2adcd369e15e2faa57845a4c0a9a
George Higbie <gehigbie@gmail.com>
2017-04-27 11:37:05 -0700

    Fix / problem

--- playground/mongo-update.js
+++ playground/mongo-update.js
@@ -1,6 +1,6 @@
 const {MongoClient, ObjectID} = require("Mongodb");
 
-MongoClient.connect("mongodb://localhost/27017/TodoApp", (err, db) => {
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     if(err){
         return Console.log("Unable to connect to DB");
     }
