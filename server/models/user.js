1b63535d6fd07f6defb34249a4fb7b3157c77805
George Higbie <gehigbie@gmail.com>
2017-04-30 10:14:48 -0700

    Comment out schema temporarily

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,7 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
-let userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
-});
-
-let User = mongoose.model("User", userSchema);
-
-// let User = mongoose.model("User", {
+// let userSchema = new Schema({
 //     email: {
 //         type: String,
 //         required: true,
@@ -20,5 +9,16 @@ let User = mongoose.model("User", userSchema);
 //         minlength: 1
 //     }
 // });
+//
+// let User = mongoose.model("User", userSchema);
+
+let User = mongoose.model("User", {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ app.post("/todos", (req, res) => {
     let todo = new Todo({
         text: req.body.text
     });
-
     todo.save().then((doc) => {
         res.send(doc);
         console.log("The todo was saved: ", doc);
@@ -21,12 +20,12 @@ app.post("/todos", (req, res) => {
         res.status(400).send(e);
         console.log("There was an error: ", e);
     });
-    console.log(req.body);
+    //console.log(req.body);
 });
 
-app.get("*", (req, res) => {
-    res.send("<h1>This page cannot be found</h1>");
-});
+// app.get("*", (req, res) => {
+//     res.send("<h1>This page cannot be found</h1>");
+// });
 
 let port = 3000
 app.listen(port, () => {
@@ -34,63 +33,3 @@ app.listen(port, () => {
 });
 
 module.exports = {app}; //use E6 export statement
-
-// let Todo = mongoose.model("Todo", {
-//     text: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true
-//     },
-//     completed:{
-//         type: Boolean,
-//         default: false
-//     },
-//     completedAt:{
-//         type: Number
-//     }
-// });
-//
-// let newTodo = new Todo({
-//     text: "Cook dinner for me!",
-//     completed: false,
-// });
-//
-// let newTodo1 = new Todo({
-//     text: "Walk the doggie!!!",
-//     completed: false,
-//     completedAt: 12
-// });
-//
-// newTodo.save().then((doc) => {
-//     console.log("Saved todo", doc);
-// }, (e) => {
-//     console.log("Unable to save todo.");
-// });
-//
-// newTodo1.save().then((doc) => {
-//     console.log("Saved todo", doc);
-// }, (e) => {
-//     console.log("Unable to save todo");
-//     console.log(e);
-// });
-//
-// let User = mongoose.model("User", {
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         minlength: 1
-//     }
-// });
-//
-// let newUser = new User({
-//     email: "bob@bob.com"
-// });
-//
-// newUser.save().then((doc) => {
-//     console.log("Saved user", doc);
-// }, (e) => {
-//     console.log("Unable to save user");
-//     console.log(e);
-// });
