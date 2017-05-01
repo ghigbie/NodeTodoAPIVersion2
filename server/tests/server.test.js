993bbbda10ca41b75cf81b5b70581ea1616ff68f
George Higbie <gehigbie@gmail.com>
2017-05-01 16:59:59 -0700

    Make small changes

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -18,17 +18,21 @@ describe("POST /todos", () => {
             .send({text})
             .expect(200)
             .expect((res) => {
-                expect((res.body.text).toBe(text));
+                expect(res.body.text).toBe(text);
             })
             .end((err, res) => {
                 if(err) {
                     return done(err);
                 }
+
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
                 }).catch(e) => done(e));
             });
+
     });
-});
+    // it("should not create a todo with invalid body data", (done) => {
+    //
+    // });
